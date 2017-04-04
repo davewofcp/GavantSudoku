be514e5ac1e85324131277e68655746ce751894d
Dave Wollyung <dave.wollyung@gmail.com>
2017-04-04 11:49:45 -0400

    Added GPL

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,3 +1,20 @@
+/*
+ * ------------------------------------------------------------------------
+ *  Copyright (c) 2017 Dave Wollyung, Clifton Park, NY, United States
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ */
+
 import java.io.IOException;
 import java.util.Scanner;
 
